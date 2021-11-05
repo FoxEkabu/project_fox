25ae0fdb0b4660332ad1b6811e00c9004281327f
FoxEkabu <Ghbdtn_2631810>
2021-11-05 19:34:35 +0500

    Домашнее задание урок 2

--- src/main/java/lesson1/program1.java
+++ src/main/java/lesson1/program1.java
@@ -1,7 +1,7 @@
 package lesson1;
 
 public class program1 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Привет ВСЕМ");
     }
 }
