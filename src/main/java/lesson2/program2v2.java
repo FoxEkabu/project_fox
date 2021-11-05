5872776d69137bfd987944a7be5ba14fff6b2708
FoxEkabu <Ghbdtn_2631810>
2021-11-05 19:32:26 +0500

    Домашнее задание урок 2 доп. задание

---
+++ src/main/java/lesson2/program2v2.java
@@ -0,0 +1,24 @@
+package lesson2;
+
+public class program2v2 {
+    public static void main(String[] args) {
+        System.out.println("Привет ВСЕМ");
+        double a = 45;
+        double b = 12;
+        System.out.println("Длина катета a=" + a);
+        System.out.println("Длина катета b=" + b);
+        // метод вычисление длины гипотенузы в прямоугольном треугольнике
+        double dlGipot = dlGipot(a,b);
+        System.out.println("Длина гипотенузы " + dlGipot);
+    }
+
+    /**
+     * @param per1 длина катета a
+     * @param per2 длина катета b
+     * @return результат длина гипотенузы
+     */
+    public static double dlGipot(double per1, double per2) {
+         ; // операция сложения
+        return Math.sqrt( Math.pow(per1, 2)+Math.pow(per2, 2));
+    }
+}
