b035439725ee63184f6843b997a9a0b5b1140beb
FoxEkabu <Ghbdtn_2631810>
2021-11-11 21:32:28 +0500

    Домашнее задание урок 3

--- src/main/java/lesson3/ProgramThree.java
+++ src/main/java/lesson3/ProgramThree.java
@@ -10,23 +10,24 @@ public class ProgramThree {
         double x1;
         double x2;
         //метод вычисление дискриминанта
-        double discriminant = discriminant(a,b,c);
+        double discriminant = discriminant(a, b, c);
         // проверка первого коэффициента на равенство нулю
         if (a == 0) {
             System.out.println("Первый коэффициент не может быть 0");
-        //проверка дискриминанта
+            //проверка дискриминанта
         } else if (discriminant < 0) {
             System.out.println("Нет действительных решений уравнения");
         } else if (discriminant == 0) {
             x1 = -1 * (b / (2 * a));
             System.out.println("Уравнение имеет один корень: " + x1);
         } else {
-            x1 = -1 * ((b+Math.sqrt(discriminant)) / (2 * a));
-            x2 = -1 * ((b-Math.sqrt(discriminant)) / (2 * a));
-            System.out.println("Уравнение имеет два корня: " +x1+" "+x2);
+            x1 = -1 * ((b + Math.sqrt(discriminant)) / (2 * a));
+            x2 = -1 * ((b - Math.sqrt(discriminant)) / (2 * a));
+            System.out.println("Уравнение имеет два корня: " + x1 + " " + x2);
         }
     }
-        public static double discriminant (double a,double b,double c) {
+
+    public static double discriminant(double a, double b, double c) {
         return (Math.pow(b, 2) - 4 * a * c);
     }
 }
