5461e997bb1b9deeef8e3e36be22fc01fce52e24
FoxEkabu <Ghbdtn_2631810>
2021-11-11 19:21:08 +0500

    Домашнее задание урок 2

--- src/main/java/lesson3/ProgramThree.java
+++ src/main/java/lesson3/ProgramThree.java
@@ -3,17 +3,18 @@ package lesson3;
 // метод вычисления корней квадратного уравнения a*x^2+b*x+c=0
 public class ProgramThree {
     public static void main(String[] args) {
-        // присваиваем коэффициенты a, b, c
-        double a = 100;
-        double b = 10;
-        double c = -50;
+        // присваиваем коэффициенты аргументам a, b, c
+        double a = 0;
+        double b = 4;
+        double c = 2;
         double x1;
         double x2;
-        //вычисление дискриминанта
-        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        //метод вычисление дискриминанта
+        double discriminant = discriminant(a,b,c);
         // проверка первого коэффициента на равенство нулю
         if (a == 0) {
             System.out.println("Первый коэффициент не может быть 0");
+        //проверка дискриминанта
         } else if (discriminant < 0) {
             System.out.println("Нет действительных решений уравнения");
         } else if (discriminant == 0) {
@@ -24,8 +25,8 @@ public class ProgramThree {
             x2 = -1 * ((b-Math.sqrt(discriminant)) / (2 * a));
             System.out.println("Уравнение имеет два корня: " +x1+" "+x2);
         }
-
-
     }
-
+        public static double discriminant (double a,double b,double c) {
+        return (Math.pow(b, 2) - 4 * a * c);
+    }
 }
---
+++ src/main/java/lesson3/ProgramTreeDop.java
@@ -0,0 +1,8 @@
+package lesson3;
+
+public class ProgramTreeDop {
+    public static void main(String[] args) {
+        int n=6;
+
+}
+}
