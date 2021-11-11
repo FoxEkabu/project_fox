04818b43a9b18bf029293050d012e5c6e429c586
FoxEkabu <Ghbdtn_2631810>
2021-11-11 21:31:20 +0500

    Домашнее задание урок 3

--- src/main/java/lesson3/ProgramTreeDop.java
+++ src/main/java/lesson3/ProgramTreeDop.java
@@ -8,12 +8,15 @@ public class ProgramTreeDop {
         System.out.println("Вычисление факториала числа n!");
         System.out.print("Введите значение n: ");
         int n = scanner.nextInt(); //Ввод и присвоение переменной n
-        int factorialsValue = 1; // присвоение первого натурального числа равное 1
-        //  Вычисление факториала
-        for (int i = 1; i <= n; i += 1) {
-            factorialsValue *= i;
-        }
-        System.out.println("Факториал числа " + n + " равно " + factorialsValue);
+        if (n<=0) {
+            System.out.println("Число n меньше или равно нулю. Вычисление факториала невозможно.");
+        } else {
+            int factorialsValue = 1; // присвоение значению факториала первого натурального числа равное 1
+            //  Вычисление факториала
+            for (int i = 1; i <= n; i += 1) {
+                factorialsValue *= i;
+            }
+               System.out.println("Факториал числа " + n + " равно " + factorialsValue);}
     }
 }
 
