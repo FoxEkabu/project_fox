99bfd59919a26b58c91dfc996169919136a423a8
FoxEkabu <Ghbdtn_2631810>
2021-11-28 17:19:43 +0500

    Домашнее задание урок 6

--- src/main/java/lesson6/ProgramSixDop.java
+++ src/main/java/lesson6/ProgramSixDop.java
@@ -17,28 +17,35 @@ public class ProgramSixDop {
         }
         int number = input.nextInt();
         // Проверка введенной переменной на равенство 0 или меньше 0
-        if (number <= 0) {
-            System.out.println("Вы ввели число меньше или равное 0");
+        if (number < 0) {
+            System.out.println("Вы ввели число меньше  0");
         } else {
             int result = 0;
-            int[] intArray = new int[number];
+            int sizeArray;
+            if (number == 0 | number == 1) {
+                sizeArray = 2;
+
+            } else {
+                sizeArray = number+1;
+            }
+            int[] intArray = new int[sizeArray];
             int indexResult;
             intArray[0] = 0;
             intArray[1] = 1;
 
-            // Высиление последовательности Фибоначчи с помещением в массив
+            // Вычисление последовательности Фибоначчи с помещением в массив
             // и элемента последовательности по порядковому номеру
             int previousResult = 0;
             int currentResult = 1;
-            for (int i = 2; i < number; i++) {
-                result = previousResult + currentResult;
-                previousResult = currentResult;
-                currentResult = result;
-                intArray[i] = result;
+            if (number != 0 & number != 1) {
+                for (int i = 2; i < sizeArray; i++) {
+                    result = previousResult + currentResult;
+                    previousResult = currentResult;
+                    currentResult = result;
+                    intArray[i] = result;
+                }
             }
-
-            System.out.println(number + " элемент последовательности Фибоначчи равен " + intArray[number - 1]);
-
+            System.out.println(number + " элемент последовательности Фибоначчи равен " + intArray[number]);
 
             System.out.println("--------------");
             //ввод второй переменной номера последовательности Фибоначчи с консоли
@@ -49,25 +56,26 @@ public class ProgramSixDop {
                 System.out.print("Повторите ввод: ");
             }
             int number2 = input.nextInt();
-            if (number2 <= 0) {
-                System.out.println("Вы ввели число меньше или равное 0");
-            } else if (number2 <= number) {
-                System.out.println(number2 + " элемент последовательности Фибоначчи равен " + intArray[number2 - 1]);
+            if (number2 < 0) {
+                System.out.println("Вы ввели число меньше 0");
+            } else if (number2 <= number | number2 == 0 | number2 == 1) {
+                System.out.println(number2 + " элемент последовательности Фибоначчи равен " + intArray[number2]);
 
             } else {
                 //копирование вычисленного массива в новый массив и вычисление продолжения последовательности Фибоначчи
-                int[] intArray2 = new int[number2];
+                int[] intArray2 = new int[number2+1];
                 System.arraycopy(intArray, 0, intArray2, 0, intArray.length);
-                previousResult = intArray2[number - 2];
-                currentResult = intArray2[number - 1];
-                for (int i = number + 1; i <= number2; i++) {
+                    previousResult = intArray2[sizeArray-2];
+
+                currentResult = intArray2[sizeArray - 1];
+                for (int i = sizeArray ; i < number2+1; i++) {
                     result = previousResult + currentResult;
                     previousResult = currentResult;
                     currentResult = result;
-                    intArray2[i - 1] = result;
+                    intArray2[i] = result;
                 }
 
-                System.out.println(number2 + " элемент последовательности Фибоначчи равен " + intArray2[number2 - 1]);
+                System.out.println(number2 + " элемент последовательности Фибоначчи равен " + intArray2[number2]);
 
             }
         }
