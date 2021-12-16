fd3ea955fe0c5b68035123e841bc9776965dff4a
FoxEkabu <Ghbdtn_2631810>
2021-12-16 14:25:08 +0500

    Домашнее задание урок 9

---
+++ src/main/java/lesson9/MaxDistance.java
@@ -0,0 +1,7 @@
+package lesson9;
+
+public final class MaxDistance {
+    public static int MAX_DISTANCE_CAR = 500;
+    public static int MAX_DISTANCE_TRACK = 1200;
+
+}
--- src/main/java/lesson9/implementation/Car.java
+++ src/main/java/lesson9/implementation/Car.java
@@ -1,20 +1,24 @@
 package lesson9.implementation;
 
 import lesson9.Automobile;
-//import lesson9.maxDistanceAutomobile.MAX_DISTANCE_CAR;
+import lesson9.MaxDistance;
 
 public class Car extends Automobile {
 
 
     public Car(String name, int distance) {
         super(name, distance);
+
     }
 
     @Override
-    public void move(String name, int distance) {
-        super.move(name, distance);
-        if (distance <= MAX_DISTANCE_CAR) {
-
+    public void move() {
+        super.move();
+        if (this.getDistance() <= MaxDistance.MAX_DISTANCE_CAR) {
+            System.out.println(" проехал дистанцию " + this.getDistance());
+        } else {
+            System.out.println(" не может проехать дистанцию " + this.getDistance());
         }
     }
 }
+
